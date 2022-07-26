9f7da664382051bbfba9ff296e840d9357e5b474
Chris Bay <chris@chrisbay.net>
2022-07-26 10:28:25 -0500

    Weather example from class

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 window.addEventListener("load", function(){
 
-});
+    fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response){
+
+    // .json() is also asynchronous 
+    response.json().then(function(data){
+      console.log(data);
+
+      let html = `
+      <h3>Weather for ${data.zipcode}:</h3>
+      <ul>
+        <li>Conditions: ${data.status}</li>
+        <li>Current temp: ${data.temp}</li>
+        <li>Chance of precipitation: ${data.chanceOfPrecipitation}</li>
+      </ul>
+      `;
+
+      document.getElementById("weather-conditions").innerHTML = html;
+
+    });
+  });
+
+// alternative asynchronous approach
+// fetch("https://handlers.education.launchcode.org/static/weather.json").then(function(response){
+
+//   // .json() is also asynchronous 
+//   return response.json(); // returns a Promise object
+  
+// }).then(function(data){
+//   console.log(data);
+
+//   let html = `
+//   <h3>Weather for ${data.zipcode}:</h3>
+//   <ul>
+//     <li>Conditions: ${data.status}</li>
+//     <li>Current temp: ${data.temp}</li>
+//     <li>Chance of precipitation: ${data.chanceOfPrecipitation}</li>
+//   </ul>
+//   `;
+
+//   document.getElementById("weather-conditions").innerHTML = html;
+
+// });
+
+  });
